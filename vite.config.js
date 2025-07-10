395c4e1cbd691b43b759f5705e87d322004c5094
Shruti <shrutisharma4440@gmail.com>
2025-07-10 10:04:11 +0530

    updated code

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  plugins: [solidPlugin()],
   test: {
-    environment: 'jsdom', // for SolidJS
+    environment: 'jsdom',
     globals: true,
+    include: ['src/**/*.{test,spec}.{js,jsx,ts,tsx}'],
     coverage: {
-      provider: 'v8', // or 'istanbul'
-      reporter: ['text', 'json', 'html'], // include JSON for automation
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
       lines: 80,
       functions: 80,
       branches: 80,
-      statements: 80
-    }
-  }
+      statements: 80,
+      include: ['src/**/*.jsx'], // 
+    },
+  },
 });
+
